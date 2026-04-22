11a406e2d1d249209b0d87094f41b0ced90146d8
farman1304 <farmanullah1107@gmail.com>
2026-04-22 09:25:27 +0500

    some changes

--- src/components/aside.js
+++ src/components/aside.js
@@ -9,7 +9,7 @@ export const Asidetitle = (p) => {
 export const Asidecontact = (p) => {
     return (
         <div className={p.c}>
-            <img src={p.src} className={p.i}></img>
+            <img alt="" src={p.src} className={p.i}></img>
             {p.t}
         </div>
     )
@@ -18,27 +18,23 @@ export const Asidecontact = (p) => {
 export const Mainpost = (p) => {
     return (
         <div className='main-post'>
-        <div className='mainpost-detailer'>
-            <div>
-                <img src={p.src} className="outline" />
+            <div className='mainpost-detailer'>
                 <div>
-                    <div className='post-id'>{p.id}</div>
-                    <div className='post-time'>{p.time} · <i className="fa-solid fa-people-group"></i></div>
+                    <img alt="" src={p.src} className="outline" />
+                    <div>
+                        <div className='post-id'>{p.id}</div>
+                        <div className='post-time'>{p.time} · <i className="fa-solid fa-people-group"></i></div>
+                    </div>
+                </div>
+                <div>
+                    <i className="fa-solid fa-ellipsis"></i>
                 </div>
             </div>
-            <div>
-                <i className="fa-solid fa-ellipsis"></i>
-            </div>
+
+            <div className='main-post-title'>{p.title}</div>
+
+            <img alt="" src={p.psrc} className='main-post-img ' />
+
         </div>
-
-        <div className='main-post-title'>{p.title}</div>
-
-        <img src={p.psrc} className='main-post-img ' />
-
-    </div>
     )
 }
-{/* <div className='aside-pages l-1'>
-            <i class="fa-solid fa-bookmark"></i>
-            Saved
-        </div> */}
--- src/components/story.js
+++ src/components/story.js
@@ -1,11 +1,11 @@
 export const Story = (props) => {
     return (
         <figure>
-            <div className="bg-img-story"><img src={props.src} height='100px' width='100px' /></div>
+            <div className="bg-img-story"><img alt="" src={props.src} height='100px' width='100px' /></div>
             <figcaption className="title">
                 {props.title}
             </figcaption>
-            <div className='pos-img-div'><img src={props.src2} className={props.cl} /></div>
+            <div className='pos-img-div'><img alt="" src={props.src2} className={props.cl} /></div>
         </figure>
     )
 }
